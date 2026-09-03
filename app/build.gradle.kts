df097bfc81cb040ba96329b6f90dddffabc887e0
OTKUSteyler <qsanom@proton.me>
2026-09-03 19:35:10 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.11"
 }
 
 android {
